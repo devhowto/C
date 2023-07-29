1c81b7b0c5b162e168bdd1f31e7ded9f9b3aef59
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-29 20:59:20 -0300

    Add return array pointer example

--- pointers/ret_arr_1.c
+++ pointers/ret_arr_1.c
@@ -1,8 +1,13 @@
+/*
+ * This example shows how to return pointer to an array from a function
+ * and then print that array.
+ */
+
 #include <stdio.h>
 
 #define LEN 5
 
-short* gen(short len) {
+short* gen(short* len) {
   static short xs[LEN] = { 1, 2, 3 };
 
   return xs;
@@ -10,7 +15,8 @@ short* gen(short len) {
 
 int main() {
   short i;
-  short* res = gen(3);
+  short len;
+  short* res = gen(&len);
 
   for (i = 0; i < 3; ++i)
     printf("%hd\n", *(res + i));
